d32d2ee2d57e9cbfc35343fef5df93d5a65e46fc
FarooqSyed0 <farooqasyed@gmail.com>
2025-06-27 23:36:13 -0700

    Delete rename.py

--- rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folder_path = '/Users/fs/Documents/DA/California SaaS Startup Growth & Funding Analysis/screenshots/new'
-
-for filename in os.listdir(folder_path):
-    old_file = os.path.join(folder_path, filename)
-    new_file = os.path.join(folder_path, filename.replace('-', '_').replace(' ', '_').lower())
-    os.rename(old_file, new_file)
-    print(f'Renamed: {old_file} -> {new_file}')
